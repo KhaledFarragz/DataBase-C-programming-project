01a76c472e1df7d22dce777977c0cbcc283cc12b
Khaled Farrag <111686450+KhaledFarragz@users.noreply.github.com>
2022-08-23 23:10:42 +0200

    Update AMIT_C_PROJECT.h

--- AMIT_C_PROJECT.h
+++ AMIT_C_PROJECT.h
@@ -6,19 +6,24 @@ struct student
     int Student_ID;
     char firstName[20];
     char SecondName[20];
-    int ID;
-    int Year;
+    int  ID;
+    int  Year;
     char C1ID[5];
-    int C1G;
+    int  C1G;
     char C2ID[5];
-    int C2G;
+    int  C2G;
     char C3ID[5];
-    int C3G;
+    int  C3G;
 }s[10];
 
 #endif
+
 void AddEntry(int, int);
+
 void ReadEntry();
+
 void SearchEntry(int);
+
 void ListIsFull();
+
 void DeleteEntry(int);
